722f3bd11020d81ab5fca02860c577b70b38aef0
Shaheer Hassan <shaheeerhassann@gmail.com>
2025-10-20 22:06:24 +0500

    Positive, Zero, or negative! problem solved

---
+++ basics/PositiveOrNegative.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        in.close();
+
+        if(num==0) System.out.println(num+" is zero");
+        else if(num>0) System.out.println(num+" is positive");
+        else System.out.println(num+" is negative");
+    }
+}
